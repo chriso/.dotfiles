de6f50fb921e0bf9cf69468989033c549572d5fd
Chris O'Hara <cohara87@gmail.com>
2012-12-07 21:16:14 +1100

    Add my base homebrew packages

---
+++ homebrew.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# See http://mxcl.github.com/homebrew/
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+
+brew update
+
+brew install ack cmake wget autoconf xz automake pkg-config scons binutils \
+    unrar zsh tmux valgrind redis nginx sqlite
+
