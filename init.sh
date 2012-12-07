acedcf0761a7de158c4f308c4c49b17bcc17927d
Chris O'Hara <cohara87@gmail.com>
2012-12-07 17:22:52 +1100

    Don't link .git

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 dir="$(cd "$(dirname "$0")" && pwd)"
 
 # Link dot files to ~
-find $dir/.[^.]* -type f -maxdepth 0 -type d -name .git -prune -o -print | while read src; do
+find $dir/.[^.]* -maxdepth 0 | grep -v '.git$' | while read src; do
     dest=$(basename $src)
     (ln -s $src ~/$dest 2>/dev/null && echo "$dest linked") || echo "$dest already exists!"
 done
