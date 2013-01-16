7df5a9cd330821babe3e105a94f700a6805179a7
Chris O'Hara <cohara87@gmail.com>
2013-01-16 13:13:15 +1100

    Update base packages

--- homebrew.sh
+++ homebrew.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# See http://mxcl.github.com/homebrew/
-ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+# Homebrew
+# http://mxcl.github.com/homebrew/
 
 brew update
 
-brew install ack cmake wget autoconf xz automake pkg-config scons binutils \
-    unrar zsh tmux valgrind redis nginx sqlite mysql git node ntfs-3g
+brew install --with-clang llvm
+
+brew install cmake wget autoconf xz automake pkg-config scons binutils \
+    unrar zsh tmux valgrind redis nginx sqlite mysql git node ntfs-3g \
+    the_silver_searcher
 
