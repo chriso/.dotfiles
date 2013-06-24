fff0b23bf50046ef4fa21ae28b464dd05068cb84
Chris O'Hara <cohara87@gmail.com>
2013-06-24 22:23:34 +1000

    Update default packages

--- homebrew.sh
+++ homebrew.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Homebrew
-# http://mxcl.github.com/homebrew/
+# Homebrew @ http://mxcl.github.com/homebrew/
 
-brew update
+brew install --with-clang --HEAD llvm
 
-brew install --with-clang llvm
-
-brew install cmake wget autoconf xz automake pkg-config scons binutils \
-    unrar zsh tmux valgrind redis nginx sqlite mysql git node ntfs-3g \
-    the_silver_searcher
+brew install cmake wget autoconf xz automake scons binutils zsh tmux \
+    valgrind redis nginx sqlite mariadb git the_silver_searcher \
+    python python3 postgresql cloc gfortran r
 
