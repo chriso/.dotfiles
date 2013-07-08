31b929000abf15ecaf4c41d83e34f3210e8f6ea6
Chris O'Hara <cohara87@gmail.com>
2013-07-09 09:38:29 +1000

    Tor time

--- homebrew.sh
+++ homebrew.sh
@@ -6,5 +6,5 @@ brew install --with-clang --HEAD llvm
 
 brew install cmake wget autoconf xz automake scons binutils zsh tmux \
     valgrind redis nginx sqlite mariadb git the_silver_searcher \
-    python python3 postgresql cloc gfortran r
+    python python3 postgresql cloc gfortran r tor
 
