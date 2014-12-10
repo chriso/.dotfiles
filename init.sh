7afe1b8122089c11aac71263b14508df011b82f5
Chris O'Hara <cohara87@gmail.com>
2014-12-11 00:30:15 +0700

    Trim some more fat

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ git submodule init
 git submodule update
 
 # Link dot files to ~
-find $dir/.[^.]* -maxdepth 0 | grep -v '.git$' | while read src; do
+find $dir/.[^.]* -maxdepth 0 | egrep -v '.git(modules)?$' | while read src; do
     dest=$(basename $src)
     (ln -s $src ~/$dest 2>/dev/null && echo "$dest linked") || echo "$dest already exists!"
 done
